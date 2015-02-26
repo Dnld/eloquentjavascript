b1ed44364e81de015e89a3972889aab33d1a8a20
Donald Steinert <steinert.donald@gmail.com>
2015-02-25 20:58:44 -0500

    completed list to array function

--- 0403alist.js
+++ 0403alist.js
@@ -1,15 +1,30 @@
 function arrayToList(array) {
-	listFromArray = {}
+	var listFromArray = {}
 	finalArrayElement = array.length - 1
 	for (var i = finalArrayElement; i >= 0; i--) {
 		if (i === finalArrayElement) {
 			listFromArray = {value: array[i], rest: null};
 		} else {
-			tempList = listFromArray;
+			var tempList = listFromArray;
 			listFromArray = {value: array[i], rest: tempList}
 		};
 	};
 	return listFromArray
 }
 
+function listToArray (listToConvert) {
+	var newArray = [];
+	
+	// for (listToConvert; listToConvert; listToConvert = listToConvert.rest) {
+	// 	newArray.push(listToConvert.value)
+	// };
+	
+	while (listToConvert) {
+		newArray.push(listToConvert.value);
+		listToConvert = listToConvert.rest
+	};
+	return newArray
+}
+
 console.log(arrayToList([10, 20, 30, 40, 50]));
+console.log(listToArray(arrayToList([10, 20, 30])));
