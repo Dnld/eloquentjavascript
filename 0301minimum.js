847864181e1b6885162491ec7ec028fde7a145c2
Donald Steinert <steinert.donald@gmail.com>
2015-03-08 12:07:47 -0400

    minimim function

---
+++ 0301minimum.js
@@ -0,0 +1,9 @@
+var min = function (a, b) {
+	return a < b ? a : b
+}
+
+console.log(min(0, 10));
+console.log(min(0, -10));
+console.log(min(1, 1));
+console.log(min(45964, 10000000));
+console.log(min(10000000, 45964));
