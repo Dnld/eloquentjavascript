fb832df584508366cefcf347a00aeb17972a5295
Donald Steinert <steinert.donald@gmail.com>
2015-06-24 19:01:08 -0400

    update chessboard solution to accept variable width and height

--- 0203chessboard.js
+++ 0203chessboard.js
@@ -1,11 +1,14 @@
 //  solution to Eloquent JavaScript chapter 2, problem 3
 //  build a chess board
 
-for (var i = 0; i < 8; i++) {
+var width = 14;
+var height = 14;
+
+for (var i = 0; i < width; i++) {
     var line = "";
     var position = ((i % 2 == 0 && " ") || "#")
     line += position;
-    for (var j = 0; j < 7; j++) {
+    for (var j = 0; j < height - 1; j++) {
         position = ((position == " " && "#") || " ");
         line += position;
     };
