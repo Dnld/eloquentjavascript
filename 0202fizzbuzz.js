cb4c0b515834aa213ac3a331e1684b32800d8f19
Donald Steinert <steinert.donald@gmail.com>
2015-06-24 18:24:11 -0400

    implemented one line solution to FizzBuzz using && and || operators

--- 0202fizzbuzz.js
+++ 0202fizzbuzz.js
@@ -5,15 +5,29 @@
 //  and "fizzBuzz" fur numbers divisible by both 3 and 5.
 
 for (var i = 1; i <= 100; i++) {
-    if (i % 3 == 0) {
-        if (i % 5 == 0) {
-            console.log("FizzBuzz");
-        } else {
-            console.log("Fizz");
-        }
-    } else if (i % 5 == 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
-    }
+    // if (i % 3 == 0) {
+    //     if (i % 5 == 0) {
+    //         console.log("FizzBuzz");
+    //     } else {
+    //         console.log("Fizz");
+    //     }
+    // } else if (i % 5 == 0) {
+    //     console.log("Buzz");
+    // } else {
+    //     console.log(i);
+    // }
+    
+    // alternative implementation
+    
+    // var output = "";
+    
+    // output += (i % 3 == 0 && "Fizz") || "";
+    // output += (i % 5 == 0 && "Buzz") || "";
+    // output = output || i;
+        
+    // one line, less readable alternative implementation
+    
+    output = (((i % 3 == 0 && ((i % 5 == 0 && "FizzBuzz") || "Fizz")) || (i % 5 == 0 && "Buzz")) || i);    
+        
+    console.log(output);
 };
